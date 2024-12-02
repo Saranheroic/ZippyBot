e393856b9f7de6a95ccaf88379c5ced10e15e3cf
saranheroic <sarann2302@gmail.com>
2024-12-03 00:44:11 +0530

    fixing svgs

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section from "./components/Section";
 import { Cover } from "@/components/ui/cover";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 
 ("use client");
 import { BackgroundBeams } from "./components/ui/background-beams";
@@ -11,7 +12,7 @@ const HomePage = () => {
       <Section
         className="pt-[12rem] -mt-[5.25rem]"
         crosses
-        crossesOffset="lg:translate-y-[5.25rem]"
+        crossesOffset="lg:translate-y-[3.95rem]"
         customPaddings
         id="hero"
       >
@@ -37,23 +38,10 @@ const HomePage = () => {
                 className="rounded-full h-32 w-32"
               />
             </div>
-
-            <div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
-              <div className="max-w-2xl mx-auto p-4">
-                <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-                  Join the waitlist
-                </h1>
-                <p></p>
-
-                <input
-                  type="text"
-                  placeholder="sarann2302@gmail.com"
-                  className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-                />
-              </div>
-            </div>
           </div>
         </div>
+
+        <BottomLine />
       </Section>
       <BackgroundBeams />
     </>

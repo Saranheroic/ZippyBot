b1a1fe4968053ea8c8b5cdbff87f9307c04d677a
saranheroic <sarann2302@gmail.com>
2025-01-31 11:39:04 +0530

    ski3

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,11 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Hero />
         <HomePage />
-        <FeaturesSectionDemo />
+        {/* <FeaturesSectionDemo /> */}
+        <Explain />
+        {/* <BackgroundGradientDemo /> */}
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
